7b0e6ae38df29ab3a2fce979dcbdaeabde677c23
Victor Monje <monjev06@gmail.com>
2026-01-24 15:27:51 -0600

    chore(user_id)
    se agrego el user id al claims

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -26,6 +26,7 @@ namespace api.Services
         {
             var claims = new List<Claim>
             {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 new Claim(JwtRegisteredClaimNames.GivenName, user.UserName)
             };
